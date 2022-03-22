aeb92f4269a232b747886f99fa4eca217a945b24
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-03-22 19:57:55 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,8 +1,7 @@
-import PropTypes from 'prop-types';
 import s from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
 
 export default function TransactionHistory({ items }) {
-  console.log(items);
   return (
     <table className={s.transactionHistory}>
       <thead>
